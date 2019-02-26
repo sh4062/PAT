e707f76dd42599007de831e1b5a33cbc98274aef
sh4062 <mx0666@163.com>
2019-02-26 10:08:23 +0800

    Update

--- A/1129.cpp
+++ A/1129.cpp
@@ -11,6 +11,60 @@
  */
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+int cmp(const pair<int, int> &a, const pair<int, int> &b)
+{
+    if (a.second != b.second)
+        return a.second > b.second;
+    return a.first < b.first;
+}
+struct node
+{
+    int key, value;
+    bool operator<(const node &a) const
+    {
+        return (value != a.value) ? value > a.value : key < a.key;
+    }
+};
+int cnt[50001] = {0};
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    set<node> ss;
+    int n, s;
+    cin >> n >> s;
+    unordered_map<int, int> maps;
+    int t;
+    cin >> t;
+    //vector<int> res;
+    cnt[t] = 1;
+    ss.insert(node{t, 1});
+    for (int i = 1; i < n; i++)
+    {
+        int tt;
+        cin >> tt;
+
+        cout << tt << ": ";
+        //vector<pair<int, int>> res(maps.begin(), maps.end());
+        //sort(res.begin(), res.end(), cmp);
+        auto it = ss.begin();
+        for (int j = 0; j < ss.size() && j < s; j++)
+        {
+            cout << (*it).key;
+            it++;
+            if (j == ss.size() - 1 || j == s - 1)
+            {
+                cout << '\n';
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        auto its = ss.find(node{tt, cnt[tt]});
+        if (its != ss.end())
+            ss.erase(its);
+        cnt[tt]++;
+        ss.insert(node{tt, cnt[tt]});
+    }
 }
